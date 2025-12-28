9506d1b5694f70778fd4d09240543299d031f7d6
Gaurav Agrawal <gauravag.1005@gmail.com>
2025-12-28 17:10:08 +0530

    feat: add AppNav and AppFooter

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import AppFooter from "./components/AppFooter"
+import AppNav from "./components/AppNav"
 
 function App() {
 
   return (
     <>
+      <AppNav />
+
+      <main>
+
+      </main>
+
+      <AppFooter />
     </>
   )
 }
---
+++ src/components/AppFooter.tsx
@@ -0,0 +1,14 @@
+export default function AppFooter() {
+  return (
+    <>
+      <footer className="bg-white shadow-sm dark:bg-gray-900">
+        <div className="w-full max-w-7xl mx-auto p-4 md:py-6">
+          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            {" "}
+            Made with 💓 over the weekend
+          </span>
+        </div>
+      </footer>
+    </>
+  );
+}
